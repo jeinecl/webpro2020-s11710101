32551831db4b43812f3ebabec8c3d379723218cc
jeinecl <jeineclaudia@gmail.com>
2020-09-03 13:15:21 +0800

    pertemuan 4

---
+++ pertemuan_04/index.js
@@ -0,0 +1,46 @@
+//Function
+// let a = 8;
+// let b = 3;
+// let c = 
+
+// let luasA = a * a;
+// let luasB = b * b;
+
+// let total = luasA + luasB;
+// console.log(total);
+
+//Function Declaration
+console.log(penjumlahanDuaBujurSangkar(8, 3));
+console.log(penjumlahanDuaBujurSangkar(10, 5));
+
+function penjumlahanDuaBujurSangkar(a, b){
+    return a * a + b * b;
+}
+
+// console.log(tambah(kali(5, 2), kali(10, 2)));
+
+// function tambah(a, b){
+//     return a + b;
+// }
+
+// function kali(a, b){
+//     return a * b;
+// }
+
+//Function Expression
+const penjumlahanDuaBujurSangkar = function (a, b){
+    return a * a + b * b;
+}
+
+console.log(penjumlahanDuaBujurSangkar(8, 3));
+console.log(penjumlahanDuaBujurSangkar(10, 5));
+
+// console.log(tambah(kali(5, 2), kali(10, 2)));
+
+// function tambah(a, b){
+//     return a + b;
+// }
+
+// function kali(a, b){
+//     return a * b;
+// }
