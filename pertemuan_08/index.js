e51458e5271b01d57e117799313652bddfeb5c48
jeinecl <jeineclaudia@gmail.com>
2020-09-23 09:55:33 +0800

    pertemuan 10 var let const

--- pertemuan_08/index.js
+++ pertemuan_08/index.js
@@ -1,29 +1,34 @@
 //Exercise 02
-let account = {
-    name: 'Jeine',
-    expenses: []
-}
+// let account = {
+//     name: 'Jeine',
+//     expenses: []
+// }
 
-function addExpenses(description, amount){
-account.expenses.push({
-    keterangan: description,
-    jumlah: amount
-});
-console.log(account.expenses[account.expenses.length-1].keterangan + ' = Rp. ' + account.expenses[account.expenses.length-1].jumlah);
-}
+// function addExpenses(description, amount){
+// account.expenses.push({
+//     keterangan: description,
+//     jumlah: amount
+// });
+// console.log(account.expenses[account.expenses.length-1].keterangan + ' = Rp. ' + account.expenses[account.expenses.length-1].jumlah);
+// }
 
-let totalExpenses = 0;
-function getAccountSummary(){
-// let totalExpenses = account.expenses * length;
-// let showTotalExpenses = function(el){
-//     console.log(el);
-account.expenses.forEach(element => {
-    totalExpenses = totalExpenses + element.jumlah;
-});
-console.log(account.name + "'s total expenses = Rp. " + totalExpenses)
-}
+// let totalExpenses = 0;
+// function getAccountSummary(){
+// // let totalExpenses = account.expenses * length;
+// // let showTotalExpenses = function(el){
+// //     console.log(el);
+// account.expenses.forEach(element => {
+//     totalExpenses = totalExpenses + element.jumlah;
+// });
+// console.log(account.name + "'s total expenses = Rp. " + totalExpenses)
+// }
 
 
-addExpenses('Ayam Geprek', 20000);
-addExpenses('Belanja Bulanan', 200000);
-getAccountSummary();
+// addExpenses('Ayam Geprek', 20000);
+// addExpenses('Belanja Bulanan', 200000);
+// getAccountSummary();
+
+// let fruits = ['Mangga', 'Pisang', 'Jambu'];
+// console.log(fruits.[2]);
+// console.log(fruits[fruits.length-1]);
+// console.log(fruits.[length-1]);
---
+++ pertemuan_10/index.js
@@ -0,0 +1,38 @@
+//CHAPTER 2
+//Var, Let, Const
+
+// let nama = "Stenly";
+// let nama = "adam";
+// console.log(nama);
+//Scope dalam javascript:
+//1. Function Scope (var)
+//2. Block scope
+
+// function tes(){
+//     for (var i=0; i<10; i++){
+//         console.log(i);
+//     }
+// }//i hanya bisa diakses di dalam function
+// tes();
+
+//bentuk IFE
+// (function tes(){
+//     for (var i=0; i<10; i++){
+//         console.log(i);
+//     }
+// })();//i hanya bisa diakses di dalam function
+// {
+//     let i;
+//     for (let i=0; i<10; i++){
+//         console.log(i);
+//     }
+// }
+// console.log(i);
+
+//Template Literal
+const person = {
+    firstName: "Stenly",
+    lastName: "Adam",
+    age: 33.
+};
+console.log(`Halo nama saya  ${person.firstName} ${person.lastName} Umur saya adalah ${person.age} tahun.`)
